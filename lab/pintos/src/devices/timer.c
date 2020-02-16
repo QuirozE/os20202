ffca61bc1363c36f7be6f3cb5a573d5fe92de5fe
equiroz <equiroz@ciencias.unam.mx>
2020-02-15 23:23:27 -0600

    Adding test changes

--- lab/pintos/src/devices/timer.c
+++ lab/pintos/src/devices/timer.c
@@ -92,8 +92,11 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  //while (timer_elapsed (start) < ticks) 
+  //  thread_yield (
+  int old = intr_set_level(INTR_OFF);
+  thread_block();
+  intr_set_level(old);
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
